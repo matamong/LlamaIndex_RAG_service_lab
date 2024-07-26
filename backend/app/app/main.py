249ed12921d616226179ccaa473d3b666c2f034e
matamong <matamatamong@gmail.com>
2024-07-26 17:33:05 +0900

    ✨ Add gpt settings

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from llama_index.core import Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
+from llama_index.llms.openai import OpenAI
 
 from app.api.api_router import api_router
 from app.utils.logging import AppLogger
@@ -16,10 +17,15 @@ logger = AppLogger().get_logger()
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info('[Main] Setting up the app...')
+    # set up the LLM and Embedding models if the OLLAMA_API_HOST is set
     logger.info('[Main] Set LLM and Embedding models.')
-    Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-    Settings.llm = Ollama(base_url=settings.OLLAMA_URL, model="llama3", request_timeout=10000.0)
+    if settings.OLLAMA_API_HOST:
+        Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+        Settings.llm = Ollama(base_url=settings.OLLAMA_URL, model="llama3", request_timeout=10000.0)
     logger.info('[Main] App setup complete.')
+    # set up the gpt version if the OPENAI_API_KEY is set
+    if settings.OPENAI_API_KEY:
+        Settings.llm = OpenAI(model="gpt-4o")
     yield
 
 app = FastAPI(lifespan=lifespan)
